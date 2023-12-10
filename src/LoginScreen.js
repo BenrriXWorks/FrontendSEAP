21ccc7ad37b7532556e58bd2a11f06dcd8e22056
BenrriXWorks <BenrriXWorks@gmail.com>
2023-12-09 22:27:12 -0300

    Sync

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -7,51 +7,55 @@ import {
   Image,
   KeyboardAvoidingView,
   Vibration,
-  Alert
+  Alert,
 } from "react-native";
 import { TextInput, Button } from "react-native-paper"; // Esto tambien? no recuerdo
 import SwitchToggle from "react-native-switch-toggle"; // Instalar esto igual
-import * as SecureStore from 'expo-secure-store'; // Aqui se necesita instalar expo-secure-store 
-
+import * as SecureStore from "expo-secure-store"; // Aqui se necesita instalar expo-secure-store
 
 // Formateador de rut
-const RutInput = ({rut, setRut}) => {
-  const [inputRut, setInputRut] = React.useState('');
+const RutInput = ({ rut, setRut }) => {
+  const [inputRut, setInputRut] = React.useState("");
 
   function formatRut(input) {
     // Elimina las letras, luego todos los puntos y guiones
-    let cleanRut = input.toUpperCase().replace(/[^0-9K.-]$/gi, '').replace(/[.-]/g, '');
-    
+    let cleanRut = input
+      .toUpperCase()
+      .replace(/[^0-9K.-]$/gi, "")
+      .replace(/[.-]/g, "");
+
     // Borra lo que haya despues de la k si hay una k
-    let posK = cleanRut.search('K') + 1;
-    if (posK)
-      cleanRut = cleanRut.substring(0, posK);
+    let posK = cleanRut.search("K") + 1;
+    if (posK) cleanRut = cleanRut.substring(0, posK);
 
     // Verifica si la longitud de los números limpios es 8 o 9
     if (cleanRut.length === 8 || cleanRut.length === 9) {
-
       // En que posiciones se colocan los puntos y el guion
-      let displacenment = [1, 4, 7]
+      let displacenment = [1, 4, 7];
       if (cleanRut.length === 9)
-        displacenment = displacenment.map((n)=>{return n+1});
+        displacenment = displacenment.map((n) => {
+          return n + 1;
+        });
 
       // Dar formato al rut
-      let formattedRut = 
-        cleanRut.substring(0, displacenment[0]) + '.' +
-        cleanRut.substring(displacenment[0],displacenment[1]) + '.' +
-        cleanRut.substring(displacenment[1],displacenment[2]) + '-' +
+      let formattedRut =
+        cleanRut.substring(0, displacenment[0]) +
+        "." +
+        cleanRut.substring(displacenment[0], displacenment[1]) +
+        "." +
+        cleanRut.substring(displacenment[1], displacenment[2]) +
+        "-" +
         cleanRut.substring(displacenment[2]);
 
       // Actualiza el estado con el RUT formateado
       setRut(formattedRut);
       setInputRut(formattedRut);
-    } 
-    else {
+    } else {
       setRut(cleanRut);
       setInputRut(cleanRut);
     }
   }
-  
+
   // Retornar el componente con su handler (formatRut)
   return (
     <TextInput
@@ -63,35 +67,32 @@ const RutInput = ({rut, setRut}) => {
   );
 };
 
-
-
 // Algoritmo de verificacion de rut chileno
-function mod11(rutStr){
-  
+function mod11(rutStr) {
   // Si no cumple el formato _.___.___-_ o __.___.___-_
   if (rutStr.length != 12 && rutStr.length != 11) return false;
 
   // Dividir el digito verificador de los numeros
-  let [numerosStr, digitoVerificadorStr] = rutStr.replaceAll(".","").split("-");
+  let [numerosStr, digitoVerificadorStr] = rutStr
+    .replaceAll(".", "")
+    .split("-");
 
   // Realizar la reduccion
-  let factor = 2, value = 0;
-  for (let i = numerosStr.length - 1; i > -1; --i){
-    value += parseInt(numerosStr.charAt(i))*(factor++);
-    if (factor > 7) 
-      factor = 2;
+  let factor = 2,
+    value = 0;
+  for (let i = numerosStr.length - 1; i > -1; --i) {
+    value += parseInt(numerosStr.charAt(i)) * factor++;
+    if (factor > 7) factor = 2;
   }
-  let residuo = 11 - value % 11; // Calcular el modulo 11
+  let residuo = 11 - (value % 11); // Calcular el modulo 11
 
   // Se desplaza el residuo y si es 0 o 1 se coloca 0 o 1, el dv debe ser 0 o k respectivamente
-  residuo = residuo === 11 ? 0 : residuo === 10 ? 'K' : residuo;
+  residuo = residuo === 11 ? 0 : residuo === 10 ? "K" : residuo;
 
   // Realizar la validación
   console.log("Resuduo: " + residuo + " , dv=" + digitoVerificadorStr);
   return residuo.toString() === digitoVerificadorStr;
-
 }
-
 
 export default function LoginScreen({ navigation }) {
   const [rut, setRut] = useState("");
@@ -99,16 +100,16 @@ export default function LoginScreen({ navigation }) {
   const [rememberMe, setRememberMe] = useState(false);
   const [initialSwitchState, setInitialSwitchState] = useState(false);
 
-  // Se colocan los estados iniciales y 
+  // Se colocan los estados iniciales y
   useEffect(() => {
     setInitialSwitchState(true); // Configura el estado inicial del switch como true
-  
+
     // Obtener datos almacenados en SecureStore al cargar la aplicación
     const getStoredData = async () => {
       try {
         const storedRut = await SecureStore.getItemAsync("rut");
         const storedPassword = await SecureStore.getItemAsync("password");
-  
+
         if (storedRut && storedPassword) {
           setRut(() => storedRut); // Actualiza el estado del RUT
           setPassword(storedPassword);
@@ -118,7 +119,7 @@ export default function LoginScreen({ navigation }) {
         console.error("Error al obtener los datos:", error);
       }
     };
-  
+
     getStoredData();
   }, []);
 
@@ -131,7 +132,7 @@ export default function LoginScreen({ navigation }) {
   // Cuando se pulsa login, verificar campos, hacer el query y pasar a la siguiente pagina
   const handleLogin = async () => {
     console.log(rut + " " + password + " " + rememberMe);
-  
+
     // Revisar los errores de entrada
     let Err = (msg) => {
       Alert.alert("Error de inicio de sesión:", "⚠️ " + msg);
@@ -139,36 +140,64 @@ export default function LoginScreen({ navigation }) {
     if (rut == "") return Err("Debes ingresar el RUT");
     if (password == "") return Err("Debes ingresar la contraseña");
     if (!mod11(rut)) return Err("El RUT ingresado es inválido");
-  
-    // Moverse a la siguiente página
-    let query = { success: true };
-    if (query.success) {
-      // Guardar las credenciales en local si rememberMe es verdadero
-      if (rememberMe) {
-        try {
-          await SecureStore.setItemAsync("rut", rut);
-          await SecureStore.setItemAsync("password", password);
-          console.log("Datos guardados de manera segura.");
-        } catch (error) {
-          console.error("Error al guardar los datos:", error);
-        }
-      }
-      else {
-        try {
-          await SecureStore.deleteItemAsync("rut");
-          await SecureStore.deleteItemAsync("password");
-          console.log("No se guardaran los datos de inicio de sesion");
-        }
-        catch(error) {
-          console.log("Err: " + error);
-        }
-      }
-      navigation.navigate("MenuPrincipal");
-    } else {
-      Alert.alert("Error de inicio de sesión: ", "RUT o contraseña inválida");
-    }
+
+    sendRequest(rut, password); // Consulta al backend
   };
 
+  const sendRequest = async (user, passw) => {
+    try {
+      /* COLOCAR AQUI LA IP DEL SERVIDOR */
+      const apiUrl = "http:// - - - /login";
+
+      if (!user || !passw) {
+        console.log("No se ha ingresado información!");
+        return;
+      }
+      const requestBody = {
+        u: user,
+        p: passw,
+      };
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
+      if (!response.ok) {
+        throw new Error("Error en la solicitud al backend");
+      }
+      const responseData = await response.json();
+      console.log("Respuesta del servidor:", responseData);
+      // Aquí puedes realizar acciones adicionales en función de la respuesta
+      if (responseData.success) {
+        console.log("Inicio de sesión exitoso");
+        if (rememberMe) {
+          try {
+            await SecureStore.setItemAsync("rut", rut);
+            await SecureStore.setItemAsync("password", password);
+            console.log("Datos guardados de manera segura.");
+          } catch (error) {
+            console.error("Error al guardar los datos:", error);
+          }
+        } else {
+          try {
+            await SecureStore.deleteItemAsync("rut");
+            await SecureStore.deleteItemAsync("password");
+            console.log("No se guardaran los datos de inicio de sesion");
+          } catch (error) {
+            console.log("Err: " + error);
+          }
+        }
+        navigation.navigate("MenuPrincipal");
+      } else {
+        Alert.alert("Error de inicio de sesión: ", "RUT o contraseña inválida");
+      }
+    } catch (error) {
+      Alert.alert("Error de inicio de sesión");
+      console.log("ERROR:", error);
+    }
+  };
 
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
@@ -219,17 +248,21 @@ export default function LoginScreen({ navigation }) {
         >
           INICIAR SESIÓN
         </Button>
-        <Text style={styles.forgotPasswordText} 
-          onPress={()=>{
-            Alert.alert("TO-DO:", "Modal de reestablecer contraseña poniendo el correo")}
-        }>
+        <Text
+          style={styles.forgotPasswordText}
+          onPress={() => {
+            Alert.alert(
+              "TO-DO:",
+              "Modal de reestablecer contraseña poniendo el correo"
+            );
+          }}
+        >
           Olvidé mi contraseña
         </Text>
       </View>
     </KeyboardAvoidingView>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
