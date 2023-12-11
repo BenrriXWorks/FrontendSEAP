8eabb818abc7cbf997f0e49642116d6a1cadef54
BenrriXWorks <BenrriXWorks@gmail.com>
2023-12-10 21:32:45 -0300

    Menu usa porcentajes para la barra superior

--- src/MenuScreen.js
+++ src/MenuScreen.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { SafeAreaView, View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 import * as SQLite from "expo-sqlite";
 
 const MenuScreen = ({ navigation, route }) => {
@@ -263,105 +263,121 @@ const MenuScreen = ({ navigation, route }) => {
   };
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#1069B4" barStyle="light-content" />
       <View style={styles.header}>
         <View style={styles.redRectangle}></View>
-        <Text style={styles.headerText}>SEAP</Text>
-        <Image
-          source={require("../assets/water-drop.png")} // Agrega la imagen en la ruta especificada
-          style={styles.waterDropImage}
-        />
+        <View style={styles.headerContent}>
+          <View style={styles.headerTextAndImage}>
+            <Text style={styles.headerText}>SEAP</Text>
+            <Image
+              source={require('../assets/water-drop.png')}
+              style={styles.waterDropImage}
+            />
+          </View>
+        </View>
       </View>
-      <Text style={styles.title}>
-        BIENVENIDO 
-        <Text style={{ color: 'orange' }}> {name.split(' ')[0].toUpperCase()}</Text>
-      </Text>
-      <TouchableOpacity style={styles.button} onPress={handleHacerVisita}>
-        <Text style={styles.buttonText}>HACER VISITA</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={handleVerVisitasEnEspera}
-      >
-        <Text style={styles.buttonText}>VER VISITAS EN ESPERA</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={handleVerMapa}>
-        <Text style={styles.buttonText}>VER MAPA</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[
-          styles.button,
-          styles.cerrarSesionButton,
-          { backgroundColor: "#EA3B44", width: "50%" },
-        ]}
-        onPress={handleCerrarSesion}
-      >
-        <Text style={[styles.buttonText, { color: "white" }]}>
-          Cerrar Sesión
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          BIENVENIDO
+          <Text style={{ color: 'orange' }}> {name.split(' ')[0].toUpperCase()}</Text>
         </Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity style={styles.button} onPress={handleHacerVisita}>
+          <Text style={styles.buttonText}>HACER VISITA</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleVerVisitasEnEspera}>
+          <Text style={styles.buttonText}>VER VISITAS EN ESPERA</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleVerMapa}>
+          <Text style={styles.buttonText}>VER MAPA</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            styles.cerrarSesionButton,
+            { backgroundColor: '#EA3B44', width: '50%' },
+          ]}
+          onPress={handleCerrarSesion}
+        >
+          <Text style={[styles.buttonText, { color: 'white' }]}>Cerrar Sesión</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#1069B4",
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: '#1069B4',
   },
   header: {
-    position: "absolute",
-    top: 70,
+    position: 'absolute',
+    top: 0,
+    left: 0,
     right: 0,
-    backgroundColor: "#EA3B44", // Fondo rojo
+    backgroundColor: '#EA3B44',
     padding: 10,
-    flexDirection: "row", // Añade una fila para alinear la imagen con el texto
-    alignItems: "center", // Alinea los elementos verticalmente
+    height: '18.4%', // El header ocupa el 20% de la pantalla verticalmente
+    flexDirection: 'row',
+    alignItems: 'flex-end', // Alinea los elementos al extremo inferior
+    justifyContent: 'flex-end', // Alinea los elementos al extremo derecho
   },
   redRectangle: {
-    position: "absolute",
-    top: -90,
-    left: -250,
-    height: 150, // Altura del rectángulo rojo
-    width: "1015%",
-    backgroundColor: "#EA3B44", // Fondo rojo
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: '100%', // Ocupa toda la altura del SafeAreaView
+    backgroundColor: '#EA3B44',
+  },
+  headerContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerTextAndImage: {
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   headerText: {
-    color: "white", // Texto blanco
-    fontWeight: "bold",
-    fontSize: 25, // Ajusta el tamaño de la fuente (puedes cambiar este valor)
-    zIndex: 1, // Asegura que el texto esté encima del rectángulo rojo
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 25,
+    marginRight: 5, // Ajusta el espacio entre el texto y la imagen
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginBottom: 20,
-    color: "white",
+    color: 'white',
   },
   button: {
-    backgroundColor: "#09b355", // Color de fondo del botón
-    borderRadius: 40, // Esquinas redondas
-    padding: 30, // Espaciado interno
-    marginBottom: 10, // Espaciado entre botones
-    width: "80%", // Ancho del botón
-    alignItems: "center", // Alineación de contenido
+    backgroundColor: '#09b355',
+    borderRadius: 40,
+    padding: 30,
+    marginBottom: 10,
+    width: '80%',
+    alignItems: 'center',
   },
   buttonText: {
-    color: "white", // Color del texto del botón
-    fontWeight: "bold",
+    color: 'white',
+    fontWeight: 'bold',
     fontSize: 18,
   },
   cerrarSesionButton: {
-    position: "absolute",
-    bottom: 20, // Ajusta la posición vertical desde la parte inferior
-    alignSelf: "center", // Ajusta la posición horizontal
+    position: 'absolute',
+    bottom: 20,
+    alignSelf: 'center',
   },
   waterDropImage: {
-    width: 40, // Ajusta el ancho de la imagen
-    height: 40, // Ajusta la altura de la imagen
-    marginLeft: 0, // Ajusta el margen izquierdo de la imagen
+    width: 60,
+    height: 60,
   },
 });
 
